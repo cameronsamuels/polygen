4e0a28cb30d0e2d84854ce96541bb3a2175bff31
Cameron Samuels <thecameron@playbclick.com>
2017-06-03 19:11:52 -0500

    Create polygen.js

---
+++ polygen.js
@@ -0,0 +1,13 @@
+var PolyGen = {};
+PolyGen.create = function(sides) {
+	PolyGen.context.clearRect(0, 0, PolyGen.canvas.width, PolyGen.canvas.height);
+	var size = PolyGen.size, x = (PolyGen.canvas.width / 2), y = (PolyGen.canvas.height / 2);
+	PolyGen.context.beginPath();
+	PolyGen.context.moveTo (x +  size * Math.cos(0), y +  size *  Math.sin(0));          
+	for (var i = 1; i <= sides;i += 1) {
+	    PolyGen.context.lineTo(x + size * Math.cos(i * 2 * Math.PI / sides), y + size * Math.sin(i * 2 * Math.PI / sides));
+	}
+  PolyGen.context.strokeStyle = PolyGen.color;
+	PolyGen.context.lineWidth = PolyGen.thickness;
+	PolyGen.context.stroke();
+};
