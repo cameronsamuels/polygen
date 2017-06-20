c850229b99345af1695d9e2e66d2cf7bbea6c463
Cameron Samuels <satchmosanders@gmail.com>
2017-06-19 20:48:09 -0500

    Fixed return token

--- bin/polygen.js
+++ bin/polygen.js
@@ -17,7 +17,8 @@ PolyGen.clear = function() {
 	PolyGen.context.clearRect(0, 0, PolyGen.canvas.width, PolyGen.canvas.height);	
 };
 PolyGen.rotate = function(r) {
-	var cv = PolyGen.canvas || return, c = PolyGen.context || cv.getContext('2d');
+	var cv = PolyGen.canvas || return;
+	c = PolyGen.context || cv.getContext('2d');
 	var width = cv.width;
 	cv.width = cv.height, cv.height = width;
 	// translate to center-canvas 
