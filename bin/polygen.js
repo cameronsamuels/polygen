8f8561d89a4e399c4f4e604c32fac6f12571d6c7
Cameron Samuels <satchmosanders@gmail.com>
2017-06-19 19:46:30 -0500

    Update polygen.js

--- bin/polygen.js
+++ bin/polygen.js
@@ -1,16 +1,16 @@
 var PolyGen = {rotate:0,color:"#000",thickness:8};
 PolyGen.create = function(s) {
-	PolyGen.context.rotate(PolyGen.rotate);
-	var context = PolyGen.context || PolyGen.canvas.getContext('2d');
-	var size = PolyGen.size || Math.min(PolyGen.width/2,PolyGen.height/2), x = (PolyGen.canvas.width / 2), y = (PolyGen.canvas.height / 2);
-	PolyGen.context.beginPath();
-	PolyGen.context.moveTo (x +  size * Math.cos(0), y +  size *  Math.sin(0));          
-	for (var i = 1; i <= s;i += 1) {
-	    PolyGen.context.lineTo(x + s * Math.cos(i * 2 * Math.PI / s), y + size * Math.sin(i * 2 * Math.PI / s));
-	}
-  	PolyGen.context.strokeStyle = PolyGen.color;
-	PolyGen.context.lineWidth = PolyGen.thickness;
-	PolyGen.context.stroke();
+	var c = PolyGen.context || PolyGen.canvas.getContext('2d');
+	c.rotate(PolyGen.rotate);
+	var sz = PolyGen.size || Math.min(PolyGen.canvas.width/2,PolyGen.canvas.height/2),
+	x = PolyGen.coords.x || (PolyGen.canvas.width / 2), y = PolyGen.coords.y || (PolyGen.canvas.height / 2);
+	c.beginPath();
+	c.moveTo (x +  sz * Math.cos(0), y +  sz *  Math.sin(0));          
+	for (var i = 1; i <= s;i += 1) c.lineTo(x + s * Math.cos(i * 2 * Math.PI / s), y + sz * Math.sin(i * 2 * Math.PI / s));
+  	c.strokeStyle = PolyGen.color;
+	c.lineWidth = PolyGen.thickness;
+	c.stroke();
+	PolyGen.context = c;
 };
 PolyGen.clear = function() {
 	PolyGen.context.clearRect(0, 0, PolyGen.canvas.width, PolyGen.canvas.height);	
