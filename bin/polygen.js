b3457eba9230140c12629e5b94c76db6b7d6bdc4
Cameron Samuels <satchmosanders@gmail.com>
2017-06-19 20:01:35 -0500

    Fixed size and sides mixup

--- bin/polygen.js
+++ bin/polygen.js
@@ -6,7 +6,7 @@ PolyGen.create = function(s) {
 	x = PolyGen.coords.x || (PolyGen.canvas.width / 2), y = PolyGen.coords.y || (PolyGen.canvas.height / 2);
 	c.beginPath();
 	c.moveTo (x +  sz * Math.cos(0), y +  sz *  Math.sin(0));          
-	for (var i = 1; i <= s;i += 1) c.lineTo(x + s * Math.cos(i * 2 * Math.PI / s), y + sz * Math.sin(i * 2 * Math.PI / s));
+	for (var i = 1; i <= s;i += 1) c.lineTo(x + sz * Math.cos(i * 2 * Math.PI / s), y + sz * Math.sin(i * 2 * Math.PI / s));
   	c.strokeStyle = PolyGen.color;
 	c.lineWidth = PolyGen.thickness;
 	c.stroke();
