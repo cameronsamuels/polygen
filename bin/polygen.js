ee13645f92892c49b4df3f038bc0e74bd37e2ffc
Cameron Samuels <satchmosanders@gmail.com>
2017-06-19 19:40:27 -0500

    Update polygen.js

--- bin/polygen.js
+++ bin/polygen.js
@@ -1,10 +1,12 @@
-var PolyGen = {};
-PolyGen.create = function(sides) {
-	var size = PolyGen.size, x = (PolyGen.canvas.width / 2), y = (PolyGen.canvas.height / 2);
+var PolyGen = {rotate:0,color:"#000",thickness:8};
+PolyGen.create = function(s) {
+	PolyGen.context.rotate(PolyGen.rotate);
+	var context = PolyGen.context || PolyGen.canvas.getContext('2d');
+	var size = PolyGen.size || Math.min(PolyGen.width/2,PolyGen.height/2), x = (PolyGen.canvas.width / 2), y = (PolyGen.canvas.height / 2);
 	PolyGen.context.beginPath();
 	PolyGen.context.moveTo (x +  size * Math.cos(0), y +  size *  Math.sin(0));          
-	for (var i = 1; i <= sides;i += 1) {
-	    PolyGen.context.lineTo(x + size * Math.cos(i * 2 * Math.PI / sides), y + size * Math.sin(i * 2 * Math.PI / sides));
+	for (var i = 1; i <= s;i += 1) {
+	    PolyGen.context.lineTo(x + s * Math.cos(i * 2 * Math.PI / s), y + size * Math.sin(i * 2 * Math.PI / s));
 	}
   	PolyGen.context.strokeStyle = PolyGen.color;
 	PolyGen.context.lineWidth = PolyGen.thickness;
