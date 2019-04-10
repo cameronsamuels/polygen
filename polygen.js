166d059a040a7544afaac890f502542689691d8b
Cameron Samuels <me@cameronsamuels.com>
2019-04-10 16:54:58 -0500

    v3.1

--- polygen.js
+++ polygen.js
@@ -1,9 +1,6 @@
-// Call this function to draw a polygon
-// Parameters: c = number of sides, a = canvas, m = canvas context,
-// e = shape radius, r = shape border thickness, o = shape border color,
-// n = shape x position , s = shape y position
-var PolyGen = function(c,a,m,e,r,o,n,s) {
-	c = c || 4; // sides
+/* PolyGen v3.1 | MIT License | git.io/fjq8z */
+function PolyGen(c,a,m,e,r,o,n,s) {
+	c = c || 3; // sides
 	if (!a && !m) return; // canvas
 	if (!m) m = a.getContext("2d"); // context
 	if (!e) e = Math.min(a.width, a.height) / 2; // radius
@@ -14,6 +11,6 @@ var PolyGen = function(c,a,m,e,r,o,n,s) {
 	
 	m.beginPath();
 	m.moveTo(n +  e * Math.cos(0), s +  e *  Math.sin(0));          
-	for (i = 1; i <= c; i++) m.lineTo(n + e * Math.cos(i * 2 * Math.PI / c), s + e * Math.sin(i * 2 * Math.PI / c));
+	for (i = 1; i <= c + 1; i++) m.lineTo(n + e * Math.cos(i * 2 * Math.PI / c), s + e * Math.sin(i * 2 * Math.PI / c));
 	m.stroke();
-};
+}
